762aaf85c75a57962674cb98dbcc5abccdc4879a
TanakaHirokazu <techiromen0@gmail.com>
2020-01-09 15:30:51 +0900

    add check_transaction check_account method

--- controller.py
+++ controller.py
@@ -1,38 +1,40 @@
 import getIncome
 import sys
 class DB_Controller(object):
-    CHECK_TRANSACTION = "1"
-    CHECK_ACCOUNT = "2"
+    TRANSACTION_MANAGER = "1"
+    ACCOUNT_MANAGER = "2"
     END_MENU = "3"
+    account_name = ""
     def __init__(self, DB_Model):
         print("hello this is controller_init_")
-        self.DB_Model = DB_Model
+        self.DB = DB_Model
 
     def menu(self):
         print("Transaction Appを起動しました")
-
+        print("あなたのアカウント名を教えてください")
+        self.DB.account_name = input()
         while True:
-            print("帳簿を確認する    1")
+            print("取引を確認する    1")
             print("口座を確認する    2")
-            print("APPを終了する　　 3")
+            print("Transaction Appを終了する　　 3")
             print("あなたがしたいことを1か2で選んでください")
             response = input()
 
-            if response == self.CHECK_TRANSACTION:
-                print("transactionを確認します")
-                # self.check_transaction()
+            if response == self.TRANSACTION_MANAGER:
+                print("transaction_managerを確認します")
+                self.DB.check_transaction()
                 
 
-            elif response == self.CHECK_ACCOUNT:
-                print("accountを確認します")
-                # self.check_account()
+            elif response == self.ACCOUNT_MANAGER:
+                print("account_managerを確認します")
+                self.DB.check_account()
 
             elif response == self.END_MENU:
                 print("システムを終了します")
                 sys.exit()
             
             else:
-                print("もう一度入力を行ってください")
+                print("----------もう一度入力を行ってください----------")
 
 
 if __name__ == "__main__":
--- getIncome.py
+++ getIncome.py
@@ -5,9 +5,11 @@ import constants
 
 
 class DB_Model(object):
+
     TRANSACTIONS_TABLE = """
         CREATE TABLE IF NOT EXISTS "transactions" (
         "id"	INTEGER NOT NULL,
+        "name"	TEXT NOT NULL UNIQUE,
         "date"	TEXT,
         "customer"	TEXT,
         "deadline"	TEXT,
@@ -22,6 +24,14 @@ class DB_Model(object):
         "money"	INTEGER
         );
         """
+    CHECK_TRANSACTION = "1"
+    CHECK_PROFIT = "2"
+    BACK_MENU = "3"
+
+    COLUMN_NAME = """
+'id'  |   'date'   |   'customer'   |  'deadline'   |  'money'
+            """
+    column = []
 
     def __init__(self, db_path):
         #DBとAPPをつなげる。
@@ -30,15 +40,48 @@ class DB_Model(object):
         self.db = sqlite3.connect(self.db_path)
         self.cursor = self.db.cursor()
 
-
     def db_init(self):
-        
+
         print("テーブルを確認なければ生成します。")
-    
+
         self.cursor.execute(self.TRANSACTIONS_TABLE)
         self.cursor.execute(self.ACCOUNT_TABLE)
 
+    def check_transaction(self):
+        print("This is check_transaction")
+        while True:
+            print("取引一覧を確認　　  　1")
+            print("利益を確認    　　　　2")
+            print("MENUに戻る　　 　　　 3")
+            print("あなたがしたいことを1,2,3で選んでください")
+            response = input()
 
+            if response == self.CHECK_TRANSACTION:
+                print("取引一覧を表示します")
+                print('{0}様の取引履歴'.format(self.account_name))
+                print(self.COLUMN_NAME)
+                self.cursor.execute("SELECT id,date,customer,deadline,money FROM transactions WHERE name=?",('{0}'.format(self.account_name),))
+                print(self.cursor.fetchall())
+
+            elif response == self.CHECK_PROFIT:
+                print("見込み利益を表示します")
+                print('{0}様の見込み利益'.format(self.account_name))
+                self.cursor.execute("SELECT")
+                
+            elif response == self.BACK_MENU:
+                print("メニューに戻ります")
+                break
+            
+            else:
+                print("もう一度入力を行ってください")
+
+
+    def check_account(self):
+        print("This is check_account")
+
+        if response == self.CHECK_ACCOUNT:
+                print("accountを確認します")
+                self.cursor.execute("SELECT * FROM account ORDER BY id")
 
     def insert_transactions_csv(self, csv_path):
 
@@ -59,8 +102,9 @@ class DB_Model(object):
 
 
 
-
 if __name__ == "__main__":    
 
     db = DB_Model("sample_db.sqlite")
     db.db_init()
+    db.account_name = "tanaka"
+    db.check_transaction()
